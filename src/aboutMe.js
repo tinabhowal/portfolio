b4bc325856e8762f9a2513f3bc91e7293f8d62d0
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-06-20 18:17:02 +0200

    changed design . image shifts to the side

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -1,148 +1,4 @@
-// import React, { useState, useEffect, useRef } from 'react';
-// import './aboutMe.css';
-// import image from './images/Frame 8.png';
-// import Contact from './contact';
-// import Projects from './projects';
-// import { Link } from 'react-router-dom';
-// import Navbar from './navbar';
 
-// function AboutMe() {
-//   const [text, setText] = useState('');
-//   const [textComplete, setTextComplete] = useState(false);
-//   const [circularpic, setCircularpic] = useState(false);
-//   const [namevisible, setNamevisible] = useState(false);
-//   const profilePictureRef = useRef(null);
- 
-  
-
-//   useEffect(() => {
-//     const message = '<HHello World!>';
-//     let currentIndex = 0;
-
-//     const timer = setInterval(() => {
-//       if (currentIndex === message.length - 1) {
-//         clearInterval(timer);
-//         setTextComplete(true);
-//         return;
-//       }
-
-//       setText((prevText) => prevText + message[currentIndex]);
-//       currentIndex++;
-//     }, 300);
-
-//     return () => {
-//       clearInterval(timer);
-//     };
-//   }, []);
-
-//   useEffect(() => {
-//     if (textComplete) {
-//       profilePictureRef.current.scrollIntoView({
-//         behavior: 'smooth',
-//         block: 'start',
-//       });
-
-//       setTimeout(() => {
-//         profilePictureRef.current.classList.add('circular');
-//         setCircularpic(true);
-//       }, 0.02);
-
-//       setTimeout(() => {
-//         setNamevisible(true);
-//       }, 2000);
-//     }
-//   }, [textComplete]);
-
-
-
-
-//   useEffect(() => {
-//     if (namevisible) {
-//       setTimeout(() => {
-//         profilePictureRef.current.classList.add('sidebar');
-//         const name = document.querySelector('.name-reveal');
-//         name.classList.add('sidebar');
-//         const container2 = document.querySelector('.container2');
-//         container2.classList.add('sidebar');
-  
-//         // Check if the name element has the sidebar class added
-         
-//         if (name.classList.contains('sidebar')) {
-//           setTimeout(() => {
-//             const paragraph = document.querySelector('.intro');
-//             paragraph.classList.add('intro-visible'); 
-//           }, 3000); 
-//         }
-//       }, 2000);
-//     }
-//   }, [namevisible]);
-  
-  
-  
-
-
-  
-
-//   return (
-//     <div className='aboutMe'>
-//        {/* <nav>
-//         <ul>
-//           <li>
-//             <Link to="/projects">Projects</Link>
-//           </li>
-//           <li>
-//             <Link to="/contact">Contact</Link>
-//           </li>
-//         </ul>
-//       </nav> */}
-
-//     <Navbar></Navbar>
-
-//       <div className={`container ${textComplete ? 'text-complete' : ''}`}>
-//         <h1>{text}</h1>
-//       </div>
-
-
-     
-//        <div className='container2'>
-//           <div
-//             ref={profilePictureRef}
-//             className={`profilePicture ${textComplete ? 'visible' : ''}`}
-//           >
-//             <img className='img' src={image} alt='profile' />
-//           </div>
-
-//           {circularpic && (
-//           <div 
-//             className='name-reveal'>
-//                 <div className='mask'></div>
-//                 <div className='name'><h2>Tina Bhawal</h2></div>
-//           </div>
-//           )}
-
-          
-//           <div className='introDiv'>
-//               <p className='intro'>
-//                 Newbie Front-end Developer <br /> 
-//                 Based in Berlin <br />
-//                 Love working on Figma<br />
-//                 Can doodle and illustrate for hours<br />
-//                 Mom to the cutest dog 
-//               </p>
-//             </div>
-           
-//      </div>
-
-     
-     
-        
-     
-
-//     </div>
-//   );
-// }
-
-// export default AboutMe;
 
 
 import React, { useState, useEffect, useRef } from 'react';
@@ -217,9 +73,32 @@ function AboutMe() {
             paragraph.classList.add('intro-visible'); 
           }, 2000); 
         }
-      }, 2000);
+      }, 1000);
     }
   }, [namevisible]);
+
+  // useEffect(() => {
+  //   if (namevisible) {
+  //     setTimeout(() => {
+  //       profilePictureRef.current.classList.add('sidebar');
+  //       const name = document.querySelector('.name-reveal');
+  //       name.classList.add('sidebar');
+  
+  //       const isNameAside = name.classList.contains('sidebar');
+  //       setNameAside(isNameAside); // Update the value of nameAside state variable
+  
+  //       if (isNameAside) {
+  //         setTimeout(() => {
+  //           const paragraph = document.querySelector('.intro');
+  //           paragraph.classList.add('intro-visible');
+  //           // const container2 = document.querySelector('.container2');
+  //           // container2.classList.add('sidebar');
+  //         }, 200);
+  //       }
+  //     }, 2000);
+  //   }
+  // }, [namevisible]);
+  
 
   return (
     <div className='aboutMe'>
@@ -242,11 +121,7 @@ function AboutMe() {
             <div className='mask'></div>
             <div className='name'>
               <h2>Tina Bhawal</h2>
-            </div>
-          </div>
-        )}
-
-        {nameAside && (
+              {nameAside && (
           <div className='introDiv'>
             <p className='intro'>
               Newbie Front-end Developer <br /> 
@@ -256,7 +131,12 @@ function AboutMe() {
               Mom to the cutest dog 
             </p>
           </div>
-         )} 
+         )}
+            </div>
+          </div>
+        )}
+
+         
       </div>
     </div>
   );
