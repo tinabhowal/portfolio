ed3a269e3065b71bca20ffde825ce336a861f56c
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-06-23 18:24:07 +0200

    added contact page

--- src/navbar.js
+++ src/navbar.js
@@ -3,20 +3,31 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 
 function Navbars() {
+  const location = useLocation();
+
   return (
-    <Navbar className="navbar" expand="lg">
+    <Navbar className="navbar"  expand="lg" variant="dark">
       <Container fluid>
         {/* <Navbar.Brand href="/">Tina Bhawal</Navbar.Brand> */}
         <Navbar.Toggle className="ml-auto" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            {/* <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link> */}
+            <Nav.Link as={Link} to="/" className={location.pathname === '/' ? 'active' : ''}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>
+              Projects
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,159 @@
 import React from 'react';
+import { useState } from 'react';
 import Navbar from './navbar';
+import './projects.css';
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import reactImage from './images/myflix client react.png';
+import oldPokemon from './images/oldPoke.png';
+import angular from './images/angular.png';
+import meetApp from './images/meetapp.png';
+import chatApp1 from './images/chatapp screenshots.jpeg';
+import firstPortfolio from './images/firstportfolio.png';
+import myFlixApi from './images/myFlixApi.png';
+import newPoke from './images/newPokemon.png';
+
 
 const Projects = () => {
+
+  const [expandedProject, setExpandedProject] = useState(null);
+
+  const projects = [
+    
+    {
+      id: 1,
+      title: 'Pokemon',
+      image: oldPokemon,
+      description:'A small web application with HTML, CSS, and JavaScript that loads data from an external API and enables the viewing of data points in detail.',
+      demoLink: 'https://tinabhowal.github.io/simple-js-app/',
+      githubLink: 'https://github.com/tinabhowal/simple-js-app',
+      
+    },
+
+    {
+      id: 2,
+      title: 'NewPokemon',
+      image: newPoke,
+      description:'The same Pokemon web application with swiper.js',
+      demoLink: '',
+      githubLink: 'https://github.com/tinabhowal/newCartoon/tree/main',
+      
+    },
+
+    {
+      id: 3,
+      title: 'myFlix-api',
+      image: myFlixApi,
+      description:'The server-side component of a “movies” web application. The web application provides users with access to information about different movies, directors, and genres. Users are able to sign up, update their personal information, and create a list of their favorite movies.',
+      // demoLink: 'https://myflix-gqp8.onrender.com',
+      githubLink: 'https://github.com/tinabhowal/movie_api',
+      
+    },
+
+    {
+      id: 4,
+      title: 'myFlix-client-React',
+      image: reactImage,
+      description:'Using React, the client-side for an app called myFlix is built based on its existing server-side code (REST API and database).',
+      demoLink: 'https://figma--mymoviesflix.netlify.app/login',
+      githubLink: 'https://github.com/tinabhowal/myFlix-client',
+      
+    },
+
+    {
+      id: 5,
+      title: 'myFlix-client-Angular',
+      image: angular,
+      description:'',
+      demoLink: 'https://tinabhowal.github.io/myFlix-Angular-client/welcome',
+      githubLink: 'https://github.com/tinabhowal/myFlix-Angular-client',
+     
+    },
+
+    {
+      id: 6,
+      title: 'MeetApp',
+      image: meetApp,
+      description:'A serverless, progressive web application (PWA) is built with React using a test-driven development (TDD) technique. The application uses the Google Calendar API to fetch upcoming events.',
+      demoLink: 'https://tinabhowal.github.io/meet/',
+      githubLink: 'https://github.com/tinabhowal/meet',
+      
+    },
+
+    {
+      id: 7,
+      title: 'ChatApp',
+      image: chatApp1,
+      description:'A chat app for mobile devices using React Native. The app provides users with a chat interface and options to share images, videos and their location.',
+      githubLink: 'https://github.com/tinabhowal/Chat-App',
+      
+    },
+
+    {
+      id: 8,
+      title: 'My first practice portfolio',
+      image: firstPortfolio,
+      description:'A responsive website using HTML and CSS that showcases my first practice portfolio.',
+      demoLink: 'https://tinabhowal.github.io/tinaprofile/careerfoundry/',
+      githubLink: 'https://github.com/tinabhowal/tinaprofile',
+      
+    },
+    
+  ];
   return (
-    <div>
-      <Navbar></Navbar>
-      <div>Projects</div>
-    </div>
+  <div className='projects'>
+  <Navbar></Navbar>
+  {/* <div className='header'>Projects</div> */}
+  <div className='body'>
+    <Container fluid>
+      <Row className='project-row'>
+        {projects.map((project) => (
+          <Col key={project.id} xs={12} md={4} className="project-col">
+            <Card  className='card'>
+              <Card.Img className="project-image" variant='top' src={project.image} />
+              <Card.Body>
+                <Card.Title>{project.title}</Card.Title>
+                {project.demoLink && (
+                  <Button className='project-button' variant='primary' href={project.demoLink}>
+                    Demo
+                  </Button>
+                )}
+                {' '}
+                <Button className='project-button' variant='dark' href={project.githubLink}>
+                  GitHub
+                </Button>{' '}
+                {expandedProject === project.id ? (
+                  <>
+                    {project.description && (
+                      <Card.Text>{project.description}</Card.Text>
+                    )}
+                    <Button
+                      className='project-button border'
+                      variant='light'
+                      onClick={() => setExpandedProject(null)}
+                    >
+                      Collapse
+                    </Button>
+                  </>
+                ) : (
+                  <Button
+                    className='border'
+                    variant='light'
+                    onClick={() => setExpandedProject(project.id)}
+                  >
+                    Project Brief
+                  </Button>
+                )}
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  </div>
+</div>
+
+      
+    
   )
 }
 
